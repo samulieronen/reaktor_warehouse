f7f4b7adb6c9f0a4dc1df72246980d5b83b4786b
Samuli <samuli841@gmail.com>
2021-02-22 15:14:33 +0200

    new frontent build

--- front/src/services/data.js
+++ front/src/services/data.js
@@ -6,13 +6,13 @@
 /*   By: seronen <seronen@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 14:20:26 by seronen           #+#    #+#             */
-/*   Updated: 2021/02/22 14:01:45 by seronen          ###   ########.fr       */
+/*   Updated: 2021/02/22 15:13:19 by seronen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import axios from 'axios'
 
-const url = 'http://localhost:3001/api/warehouse/'
+const url = '/api/warehouse/'
 
 const fetch = (product) => {
     console.log(`Fetching ${product} data!`)
