221ba95d7ee550c85342e6b5cfb65cc33b214063
Samuli <samuli841@gmail.com>
2021-02-22 15:23:09 +0200

    protected refresh_rate

--- api/utils/config.js
+++ api/utils/config.js
@@ -6,7 +6,7 @@
 /*   By: seronen <seronen@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/07 12:51:55 by seronen           #+#    #+#             */
-/*   Updated: 2021/02/22 14:44:27 by seronen          ###   ########.fr       */
+/*   Updated: 2021/02/22 15:22:11 by seronen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,14 @@ FORCE_ERROR = process.env.FORCE_ERROR || 'false'
 REFRESH_RATE = Number(process.env.REFRESH_RATE) || 330000 // 5.5 mins
 MAX_TRIES = Number(process.env.MAX_TRIES) || 6
 
+if (REFRESH_RATE < 60000) {
+	REFRESH_RATE = 330000
+}
 
 HEADERS = {
 	'content-type': 'application/json'
 }
+
 if (FORCE_ERROR === 'true') {
 	console.log('Header \'x-force-error-mode\' in effect!')
 	HEADERS = {
