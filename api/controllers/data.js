c05e98347853e723a8b95fc6d569db2d7bf3e05f
Samuli <samuli841@gmail.com>
2021-02-23 13:28:11 +0200

    error handling improvements

--- api/controllers/data.js
+++ api/controllers/data.js
@@ -6,7 +6,7 @@
 /*   By: seronen <seronen@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 01:35:14 by seronen           #+#    #+#             */
-/*   Updated: 2021/02/22 14:45:20 by seronen          ###   ########.fr       */
+/*   Updated: 2021/02/23 13:22:43 by seronen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,20 @@ const config = require('../utils/config')
 const fetchAllProducts = async () => {
 	const [beanies, facemasks, gloves] = await Promise.all([
 		fetch(`${config.BASE_URL}products/beanies`, { method: 'GET', headers: config.HEADERS})
-			.then(response => response.json()
-			.catch(error => console.log(error))),
+			.then(response => {
+				return (response.ok) ? response.json() : []
+			})
+			.catch(error => console.log(error)),
 		fetch(`${config.BASE_URL}products/facemasks`, { method: 'GET', headers: config.HEADERS})
-			.then(response => response.json()
-			.catch(error => console.log(error))),
+		.then(response => {
+			return (response.ok) ? response.json() : []
+		})
+		.catch(error => console.log(error)),
 		fetch(`${config.BASE_URL}products/gloves`, { method: 'GET', headers: config.HEADERS})
-			.then(response => response.json()
-			.catch(error => console.log(error)))
+		.then(response => {
+			return (response.ok) ? response.json() : []
+		})
+		.catch(error => console.log(error))
 	])
 	return {
 		beanies,
@@ -35,7 +41,9 @@ const fetchAllProducts = async () => {
 const fetchAllAvailabilities = async (items) => {
 	const availability = await Promise.all(items.map(item => 
 		fetch(`${config.BASE_URL}availability/${item}`, { method: 'GET', headers: config.HEADERS})
-			.then(response => response.json())
+			.then(response => {
+				return (response.ok) ? response.json() : []
+			})
 			.catch(error => console.log(error))))
 	return availability
 }
